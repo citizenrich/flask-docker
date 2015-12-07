aabcc53d2ebc4a03574479a9964d6a8b2cb05d77
Richard Stanley <richard@thrillride.local>
2015-12-07 23:44:27 +0300

    fabfile auto-commit

--- fabfile.py
+++ fabfile.py
@@ -27,5 +27,3 @@ def deploy():
         run('start telephony') #for upstart
         run('restart telephony') #for upstart
         run('service nginx restart')
-        #run("ps -ef | grep 'gunicorn' | grep -v grep | awk '{print $2}' | xargs kill -9")
-        #run('gunicorn -b 127.0.0.1:8000 --workers=5 -m 007 wsgi')
